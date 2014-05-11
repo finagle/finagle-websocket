e6e3fae2419c0d9dc14fd7511c38aa66346b44aa
Jeff Smick <jeff@squishtech.com>
2014-05-11 14:50:37 -0700

    setup the build file

---
+++ project/Build.scala
@@ -0,0 +1,68 @@
+import sbt._
+import Keys._
+
+object FinagleLibs extends Build {
+  val libVersion = "6.15.0"
+
+  val baseSettings = Defaults.defaultSettings ++ Seq(
+    libraryDependencies ++= Seq(
+      "com.twitter" %% "finagle-core" % libVersion,
+      "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+      "junit" % "junit" % "4.8.1" % "test"
+    )
+  )
+
+  lazy val buildSettings = Seq(
+    organization := "com.github.sprsquish",
+    version := libVersion,
+    crossScalaVersions := Seq("2.9.2", "2.10.0")
+  )
+
+  lazy val publishSettings = Seq(
+    publishMavenStyle := true,
+    publishArtifact := true,
+    publishTo := Some(Resolver.file("localDirectory", file(Path.userHome.absolutePath + "/workspace/mvn-repo"))),
+    licenses := Seq("Apache 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
+    homepage := Some(url("https://github.com/sprsquish/finagle-libs")),
+    pomExtra := (
+      <scm>
+        <url>git://github.com/sprsquish/finagle-libs.git</url>
+        <connection>scm:git://github.com/sprsquish/finagle-libs.git</connection>
+      </scm>
+        <developers>
+          <developer>
+            <id>sprsquish</id>
+            <name>Jeff Smick</name>
+            <url>https://github.com/sprsquish</url>
+          </developer>
+        </developers>)
+  )
+
+  def finProject(n: String): Project = {
+    val name = "finagle-" + n
+    Project(
+      id = name,
+      base = file(name),
+      settings =
+        Defaults.itSettings ++
+        baseSettings ++
+        buildSettings ++
+        publishSettings
+    ).configs(IntegrationTest)
+  }
+
+  lazy val finagleLibs = Project(
+    id = "finagle-libs",
+    base = file("."),
+    settings = Project.defaultSettings
+  ).aggregate(finagleIrc, finagleWebsocket)
+
+  lazy val finagleIrc =
+    finProject("irc").settings(
+      libraryDependencies ++= Seq(
+        "com.twitter" %% "twitter-server" % "1.6.1")
+    )
+
+  lazy val finagleWebsocket =
+    finProject("websocket")
+}
